7fbe47124b91b9c979167c6e6c4deb336b23f213
marit pedersen <marit_jp@hotmail.com>
2021-05-19 21:57:02 +0200

    t

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,23 +2,24 @@ import React from 'react';
 
 class LoginForm extends React.Component {
     state = {
-        email: '',
-        password: ''
+        email: ' ',
+        password: ' '
     };
 
     handleChange = event => {
         this.setState({ [event.target.name]: event.target.value });
-    }
+    };
 
     handleSubmit = event => {
         event.preventeDefault();
         console.log(this.state);
-    }
+    };
     
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <div><input type="email"
+                <div>
+                    <input type="email"
                 name="email" 
                 placeholder="email"
                 onChange={this.handleChange}
@@ -27,11 +28,12 @@ class LoginForm extends React.Component {
                 name="password" 
                 placeholder="password"
                 onChange={this.handleChange}
-                 /></div>
-                <button type="submit" text="submit"></button>
+                 />
+                 </div>
+                <button type="submit">Submit</button>
             </form>
 
-        )
+        );
     }
 }
 
